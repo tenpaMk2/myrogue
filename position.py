d812b2b7bef35442e572195a289d90003367c829
tenpaMk2 <wavyroom@gmail.com>
2015-06-12 00:37:04 +0900

    NUM_TO_DIRECTION定数のおかげで、get_direction_by_characterの実装が簡単に。FIXME追加。

--- position.py
+++ position.py
@@ -2,12 +2,15 @@
 # -*- coding:utf-8 -*-
 __author__ = 'tenpa'
 
-NUM_DIRECTION = dict(enumerate([
+# FIXME 方角が文字の場合と単語の場合とで別々に生成するのは良くなさそう。一括で管理できないかな。
+
+NUM_TO_DIRECTION = dict(enumerate([
     'n',
     'e',
     's',
     'w']))
-DIRECTION_NUM = dict(zip(NUM_DIRECTION.values(), NUM_DIRECTION.keys()))
+DIRECTION_TO_NUM = dict(zip(NUM_TO_DIRECTION.values(), NUM_TO_DIRECTION.keys()))
+DIRECTION_WORD = ['North', 'East', 'South', 'West']
 
 DIRECTION_MOVE = ([-1, 0], [0, 1], [1, 0], [0, -1])
 
@@ -15,23 +18,23 @@ DIRECTION_MOVE = ([-1, 0], [0, 1], [1, 0], [0, -1])
 class PositionAndDirectionFactory(object):
     @staticmethod
     def make_north_p(position):
-        return PositionAndDirection(position, DIRECTION_NUM['n'])
+        return PositionAndDirection(position, DIRECTION_TO_NUM['n'])
 
     @staticmethod
     def make_east_p(position):
-        return PositionAndDirection(position, DIRECTION_NUM['e'])
+        return PositionAndDirection(position, DIRECTION_TO_NUM['e'])
 
     @staticmethod
     def make_south_p(position):
-        return PositionAndDirection(position, DIRECTION_NUM['s'])
+        return PositionAndDirection(position, DIRECTION_TO_NUM['s'])
 
     @staticmethod
     def make_west_p(position):
-        return PositionAndDirection(position, DIRECTION_NUM['w'])
+        return PositionAndDirection(position, DIRECTION_TO_NUM['w'])
 
 
 class PositionAndDirection(object):
-    def __init__(self, position, direction=DIRECTION_NUM['n']):
+    def __init__(self, position, direction=DIRECTION_TO_NUM['n']):
         self.position = position
         self.direction = direction
 
@@ -42,28 +45,28 @@ class PositionAndDirection(object):
         self.position[1] += DIRECTION_MOVE[direction][1]
 
     def move_north(self):
-        self.__move_towards(DIRECTION_NUM['n'])
+        self.__move_towards(DIRECTION_TO_NUM['n'])
 
     def move_east(self):
-        self.__move_towards(DIRECTION_NUM['e'])
+        self.__move_towards(DIRECTION_TO_NUM['e'])
 
     def move_south(self):
-        self.__move_towards(DIRECTION_NUM['s'])
+        self.__move_towards(DIRECTION_TO_NUM['s'])
 
     def move_west(self):
-        self.__move_towards(DIRECTION_NUM['w'])
+        self.__move_towards(DIRECTION_TO_NUM['w'])
 
     def turn_north(self):
-        self.direction = DIRECTION_NUM['n']
+        self.direction = DIRECTION_TO_NUM['n']
 
     def turn_east(self):
-        self.direction = DIRECTION_NUM['e']
+        self.direction = DIRECTION_TO_NUM['e']
 
     def turn_south(self):
-        self.direction = DIRECTION_NUM['s']
+        self.direction = DIRECTION_TO_NUM['s']
 
     def turn_west(self):
-        self.direction = DIRECTION_NUM['w']
+        self.direction = DIRECTION_TO_NUM['w']
 
     def run(self):
         self.__move_towards(self.direction)
@@ -99,7 +102,10 @@ class PositionAndDirection(object):
             raise ValueError("direction must be [0, 3]")
         self._direction = direction
 
-    def get_direction_by_charcter(self):
-        for key in DIRECTION_NUM:
-            if DIRECTION_NUM[key] == self.direction:
-                return key
+    def get_direction_by_character(self):
+        return NUM_TO_DIRECTION[self.direction]
+
+    # def get_direction_by_word(self):
+    #     for key in DIRECTION_TO_NUM:
+    #         if DIRECTION_TO_NUM[key] == self.direction:
+    #             return key
