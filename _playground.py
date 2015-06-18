f9910b9b4c5f28eedd0aae7748ffb94b1d91d5fa
tenpaMk2 <wavyroom@gmail.com>
2015-06-19 01:47:22 +0900

    pygameのインストール。pygameのチュートリアルコードが動くことを確認。

--- _playground.py
+++ _playground.py
@@ -134,8 +134,8 @@ ttv.run()
 # nip = NoImplementer()  # TypeError: Can't instantiate abstract class TTvillager with abstract methods _end__turn
 tth.print_name_multiple_times()
 
-
 print("--------------------------------")
+
 
 class MetaclassTesuto(metaclass=ABCMeta):
     def __init__(self):
@@ -144,6 +144,36 @@ class MetaclassTesuto(metaclass=ABCMeta):
     def metaclass_ga_arudake(self):
         print("まあでも別にいいかなぁー?")
 
+
 metaclasstesuto = MetaclassTesuto()
 metaclasstesuto.metaclass_ga_arudake()
 
+print("-----pygame-----------")
+import sys, pygame
+
+pygame.init()
+
+size = width, height = 320, 240
+speed = [2, 2]
+black = 0, 0, 0
+
+screen = pygame.display.set_mode(size)
+
+ball = pygame.image.load("burn.jpg")
+ballrect = ball.get_rect()
+
+while 1:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: sys.exit()
+
+    ballrect = ballrect.move(speed)
+    if ballrect.left < 0 or ballrect.right > width:
+        speed[0] = -speed[0]
+    if ballrect.top < 0 or ballrect.bottom > height:
+        speed[1] = -speed[1]
+
+    screen.fill(black)
+    screen.blit(ball, ballrect)
+    pygame.display.flip()
+
+print("終了")
