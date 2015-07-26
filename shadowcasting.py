100e29a3527c809ecdb9afdbcad13b9420ff5d5c
tenpaMk2 <wavyroom@gmail.com>
2015-07-26 23:17:31 +0900

    cursesから分離したり、リネームしたり、コメント追加したり。あとは本体とどう組み合わせるかだなあ。

--- shadowcasting.py
+++ shadowcasting.py
@@ -2,34 +2,22 @@
 # -*- coding:utf-8 -*-
 __author__ = 'tenpa'
 
-# FOV calculation for roguelike
+import logging
+import logging.config
 
-import curses
+logging.config.fileConfig("config/logging.conf")
 
 FOV_RADIUS = 10
-
-dungeon = ["###########################################################",
-           "#...........#.............................................#",
-           "#...........#........#....................................#",
-           "#.....................#...................................#",
-           "#....####..............#..................................#",
-           "#.......#.......................#####################.....#",
-           "#.......#...........................................#.....#",
-           "#.......#...........##..............................#.....#",
-           "#####........#......##..........##################..#.....#",
-           "#...#...........................#................#..#.....#",
-           "#...#............#..............#................#..#.....#",
-           "#...............................#..###############..#.....#",
-           "#...............................#...................#.....#",
-           "#...............................#...................#.....#",
-           "#...............................#####################.....#",
-           "#.........................................................#",
-           "#.........................................................#",
-           "###########################################################"]
 
 
 # 座標は下向き正の右向き生。
 # octantは左上の部分を基準に考える。それゆえに、扱う座標が常に負の値を取ってしまいムズムズする。
+
+# FIXME astarの関数と同じもの。2次元のリスト用のモジュールを作った方が良さげ。
+def print_nested_list(nested_list: list):
+    formatted_str = "\n".join(["".join(row) for row in nested_list])
+    logging.debug('\n' + formatted_str)
+
 
 class FOVMap(object):
     # Multipliers for transforming coordinates to other octants:
@@ -43,10 +31,13 @@ class FOVMap(object):
     def __init__(self, parsed_map):
         self.parsed_map = parsed_map
         self.width, self.height = len(parsed_map[0]), len(parsed_map)
-        self.light = []
-        for i in range(self.height):
-            self.light.append([0] * self.width)
-        self.flag = 0
+        self.is_in_fov_flags = self.make_is_in_fov_flags()
+
+    def clear_is_in_fov_flags(self):
+        self.is_in_fov_flags = self.make_is_in_fov_flags()
+
+    def make_is_in_fov_flags(self):
+        return [[False for _ in range(self.width)] for _ in range(self.height)]
 
     def get_map_cell(self, x, y):
         return self.parsed_map[y][x]
@@ -56,20 +47,26 @@ class FOVMap(object):
                 or x >= self.width or y >= self.height
                 or self.parsed_map[y][x] == "#")
 
-    def is_litted(self, x, y):
-        return self.light[y][x] == self.flag
+    def is_in_fov(self, x, y):
+        return self.is_in_fov_flags[y][x]
 
-    def set_lit(self, x, y):
+    def set_in_fov(self, x, y):
         if 0 <= x < self.width and 0 <= y < self.height:
-            self.light[y][x] = self.flag
+            self.is_in_fov_flags[y][x] = True
 
     def _cast_light(self, cx, cy, start_row, start_slope, end_slope, radius, xx, xy, yx, yy, oct_id):
         """Recursive lightcasting function"""
+
+        logging.info("start_slope : %r", start_slope)
+        logging.info("end_slope : %r", end_slope)
+
         if start_slope < end_slope:
+            logging.info("start_slope < end_slope")
             return
 
-        # radiusは比較するだけなので、平方根を取らなくていい。
+        # 後で比較するだけなので、radius_squaredは平方根を取らなくていい。
         radius_squared = radius * radius
+
         for row in range(start_row, radius + 1):
             # dxとdyはslopeの計算用。dx = x1 - x2 を示す。
             # dxが-2から始まるのは、whileで最初に+1するため。
@@ -90,6 +87,7 @@ class FOVMap(object):
                 l_slope = (dx - 0.5) / (dy + 0.5)
                 r_slope = (dx + 0.5) / (dy - 0.5)
 
+                # 俺が追加したslope。一個前の操作のときのr_slope。
                 previous_r_slope = (dx - 0.5) / (dy - 0.5)
 
                 if r_slope > start_slope:
@@ -100,96 +98,101 @@ class FOVMap(object):
 
                 else:
                     # Our light beam is touching this square; light it:
-                    if dx * dx + dy * dy < radius_squared:
-                        self.set_lit(trans_x, trans_y)
+                    # つまり、start_slopeからend_slopeの間の勾配であり、radius以内であれば視界に入ったと見なす。
+                    if (dx ** 2) + (dy ** 2) < radius_squared:
+                        self.set_in_fov(trans_x, trans_y)
 
                     if is_previous_cell_blocked_flag:
+                        # 一個前の捜査のとき、ブロックであった場合
                         # we're scanning a row of blocked squares:
                         if self.is_blocked(trans_x, trans_y):
-                            # new_start_slope = r_slope
+                            # ブロックが続いている場合
                             continue
                         else:
+                            # ブロックが途切れた場合、start_slopeを設定。
                             is_previous_cell_blocked_flag = False
                             start_slope = previous_r_slope
                     else:
+                        # 一個前の捜査のとき、ブロックではなかった場合
                         if self.is_blocked(trans_x, trans_y) and row < radius:
+                            # ブロックだった場合、新たなスキャンを開始（新たなスキャンを先に計算する。）
                             # This is a blocking square, start a child scan:
                             is_previous_cell_blocked_flag = True
                             self._cast_light(cx, cy, row + 1, start_slope, l_slope,
                                              radius, xx, xy, yx, yy, oct_id + 1)
-                            # new_start_slope = r_slope
 
             # Row is scanned; do next row unless last square was blocked:
             if is_previous_cell_blocked_flag:
+                # 行のスキャンの最後がブロックで終わった場合、スキャン全体を終了する。
+                logging.info("block end")
                 break
 
     def do_fov(self, cx, cy, radius):
-        """Calculate lit squares from the given location and radius"""
-        self.flag += 1
+        """FOVの計算"""
+
+        # 視界のクリア。忘れると視界がオンになりっぱなしな部分ができる。
+        self.clear_is_in_fov_flags()
+
+        # 8つの3角形に分けて計算。wiki参照。
         for octant in range(len(self.mult[0])):
+            logging.info("----------start octant : %r----------", octant)
             self._cast_light(cx, cy, 1, 1.0, 0.0, radius,
                              self.mult[0][octant], self.mult[1][octant],
                              self.mult[2][octant], self.mult[3][octant], 0)
 
-    def display(self, curses_scr, current_x, current_y):
-        """Display the map on the given curses screen (utterly unoptimized)"""
-        dark_color, lit_color = curses.color_pair(8), curses.color_pair(7) | curses.A_BOLD
-        for x in range(self.width):
-            for y in range(self.height):
-                if self.is_litted(x, y):
-                    attr = lit_color
-                else:
-                    attr = dark_color
+    def display(self, current_x, current_y):
+        """マップの表示"""
 
-                if x == current_x and y == current_y:
-                    ch = '@'
-                    attr = lit_color
-                else:
-                    ch = self.get_map_cell(x, y)
-                curses_scr.addstr(y, x, ch, attr)
-        curses_scr.refresh()
+        map_buffer = [
+            [
+                '@' if (x == current_x and y == current_y) else
+                self.get_map_cell(x, y) if self.is_in_fov(x, y) else
+                ' '
+                for x in range(self.width)] for y in range(self.height)]
 
-
-def color_pairs():
-    c = []
-    for i in range(1, 16):
-        curses.init_pair(i, i % 8, 0)
-        if i < 8:
-            c.append(curses.color_pair(i))
-        else:
-            c.append(curses.color_pair(i) | curses.A_BOLD)
-    return c
+        print_nested_list(map_buffer)
 
 
 if __name__ == '__main__':
-    try:
-        s = curses.initscr()
-        curses.start_color()
-        curses.noecho()
-        curses.cbreak()
-        color_pairs()
-        s.keypad(1)
+    map_data = [
+        "###########################################################",
+        "#...........#.............................................#",
+        "#...........#........#....................................#",
+        "#.....................#...................................#",
+        "#....####..............#..................................#",
+        "#.......#.......................#####################.....#",
+        "#.......#...........................................#.....#",
+        "#.......#...........##..............................#.....#",
+        "#####........#......##..........##################..#.....#",
+        "#...#...........................#................#..#.....#",
+        "#...#............#..............#................#..#.....#",
+        "#...............................#..###############..#.....#",
+        "#...............................#...................#.....#",
+        "#...............................#...................#.....#",
+        "#...............................#####################.....#",
+        "#.........................................................#",
+        "#.........................................................#",
+        "###########################################################"
+    ]
 
-        d_current_x, d_current_y = 36, 13
-        d_fov_map = FOVMap(dungeon)
-        while True:
-            d_fov_map.do_fov(d_current_x, d_current_y, FOV_RADIUS)
-            d_fov_map.display(s, d_current_x, d_current_y)
+    d_current_x, d_current_y = 36, 13
+    d_fov_map = FOVMap(map_data)
 
-            key = s.getch()
-            if key == 27:
-                break
-            elif key == 259:
-                d_current_y -= 1
-            elif key == 258:
-                d_current_y += 1
-            elif key == 260:
-                d_current_x -= 1
-            elif key == 261:
-                d_current_x += 1
-    finally:
-        s.keypad(0)
-        curses.echo()
-        curses.nocbreak()
-        curses.endwin()
-        print("Normal termination.")
+    while True:
+        d_fov_map.do_fov(d_current_x, d_current_y, FOV_RADIUS)
+        d_fov_map.display(d_current_x, d_current_y)
+
+        key = input()
+
+        if key == 'n':
+            d_current_y -= 1
+        elif key == 's':
+            d_current_y += 1
+        elif key == 'e':
+            d_current_x += 1
+        elif key == 'w':
+            d_current_x -= 1
+        else:
+            raise Exception("Invalid input.")
+
+        logging.debug("key : %r", key)
